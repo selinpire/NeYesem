b7de2f0f7532532cc428a87a47d8fd619b396dba
Sude Kocak <sudekck.24@gmail.com>
2026-04-04 19:27:24 +0300

    Vercel2

--- SudeKocak/Sudebackend/server.js
+++ SudeKocak/Sudebackend/server.js
@@ -8,7 +8,7 @@ const routes = require("./app_api/routes/index");
 
 const app = express();
 
-app.use(cors({ origin: "https://ne-yesem-amber.vercel.app/" }));
+app.use(cors({ origin: "http://localhost:5173" }));
 // Base64 tarif görseli JSON ile gönderildiği için limit yükseltilir (varsayılan ~100kb → 413)
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
